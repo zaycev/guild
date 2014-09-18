0d7265cbca1992a1820f1617af11a93488481918
Zaitsev <zaytsev@usc.edu>
2014-09-17 19:38:15 -0700

    Added import of current dir

--- ldt/wsgi.py
+++ ldt/wsgi.py
@@ -2,6 +2,9 @@
 # Author: Vova Zaytsev <zaytsev@usc.edu>
 
 import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ldt.settings")
 
 from django.core.wsgi import get_wsgi_application
