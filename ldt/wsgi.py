0290a13c0f41b35ede96a07081c91fb6253f2618
Zaitsev <zaytsev@usc.edu>
2014-09-17 19:40:44 -0700

    Added import of current dir

--- ldt/wsgi.py
+++ ldt/wsgi.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath('__file__')), ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ldt.settings")
 
 from django.core.wsgi import get_wsgi_application
