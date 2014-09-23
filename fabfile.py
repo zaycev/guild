f75a84b14b3e548d451569dd0cbe7ac3783fb866
Zaitsev <zaytsev@usc.edu>
2014-09-23 16:56:17 -0700

    Update

--- fabfile.py
+++ fabfile.py
@@ -13,21 +13,15 @@ import json
 import glob
 import fabric
 
-from fabric.api import *
-from fabric.colors import green, red
+from fabric.api import cd
+from fabric.api import env
+from fabric.colors import red
+from fabric.colors import green
+from fabric.operations import run
+from fabric.operations import sudo
 
 
-PYPY_UWSGI_BIN = "/usr/lib/pypy/bin/uwsgi"
-
-UBUNTU_PACKAGES = [
-    "git",
-    "uwsgi",
-    "nginx",
-    "pypy",
-    "pypy-dev",
-    "uwsgi-plugin-python",
-    "postgresql-server-dev-9.3",
-]
+UBUNTU_PACKAGES = open("ubuntu.txt", "rb").read().split("\n")
 
 
 def dev():
@@ -59,7 +53,7 @@ def run_local():
 
 
 def server():
-    env.host_string  = "107.170.210.184"
+    env.host_string  = "162.243.152.239"
     env.user         = "root"
     env.key_filename = "~/.ssh/id_rsa"
     env.local        = False
@@ -69,24 +63,34 @@ def init():
     config = env.config
     if not env.local:
 
-        # print(green("Installing packages."))
+        print(green("Installing packages."))
+        run("aptitude update > /dev/null")
+        run("aptitude upgrade > /dev/null")
+        run("aptitude -f install %s > /dev/null" % " ".join(UBUNTU_PACKAGES))
 
-        print(green("Clonning repository."))
-        run("aptitude update")
-        run("aptitude -f install %s" % " ".join(UBUNTU_PACKAGES))
+        if not fabric.contrib.files.exists(env.config["path"]):
+            print(green("Clonning repository."))
+            run("git clone {repository} -b {branch} {path}".format(**env.config))
+        else:
+            print(green("Repository already exists"))
 
         print(green("Installing python requirements."))
-        if not fabric.contrib.files.exists(env.config["path"]):
-            run("git clone {repository} -b {branch} {path}".format(**env.config))
-        run("pypy -m pip install -r {path}/requirements.txt".format(**config))
+        run("pip install -r {path}/requirements.txt > /dev/null".format(**config))
 
-        print(green("Compiling additional requirements."))
-        run("mkdir -p {path}/build".format(**config))
-        run("curl http://uwsgi.it/install | bash -s pypy {path}/uwsgi".format(**config))
+        print(green("Setting up logs."))
+        run("mkdir -p {logs}".format(**config))
+        run("mkdir -p {path}/logs".format(**config))
+        run("chown root:root -R {logs}".format(**config))
+        run("chown root:root -R {path}".format(**config))
 
-        print(green("Compiling additional requirements."))
-        run("curl http://uwsgi.it/install | bash -s pypy {path}/uwsgi".format(**config))
-        run("bzip2 < libpypy.so.bz2 > libpypy.so")
+
+
+def update_ubuntu():
+    with open("ubuntu.txt", "r") as i_fl:
+        ubuntu_packages = i_fl.read().split("\n")
+    sudo("aptitude update")
+    sudo("aptitude install %s" % " ".join(ubuntu_packages))
+
 
 
 def update():
@@ -117,10 +121,13 @@ def deploy():
         run("git pull")
 
         print(green("Fetching requirements."))
-        run("pypy -m pip install -r {path}/requirements.txt".format(**config))
+        run("pip install -r {path}/requirements.txt > /dev/null".format(**config))
 
         print(green("Setting up logs."))
         run("mkdir -p {logs}".format(**config))
+        run("mkdir -p {path}/logs".format(**config))
+        run("chown root:root -R {logs}".format(**config))
+        run("chown root:root -R {path}".format(**config))
 
 
         print(green("Uploading setting.py"))
@@ -130,28 +137,29 @@ def deploy():
                                              use_jinja=True)
 
         print(green("Uploading usgi.ini"))
-        # fabric.contrib.files.upload_template("fab/uwsgi.ini",
-        #                                      "{path}/uwsgi.ini".format(**config),
-        #                                      context=context,
-        #                                      use_jinja=True)
-        # run("cp -f {path}/uwsgi.ini /etc/uwsgi/apps-available/{stage}.ini".format(**config))
-        # run("ln -sf /etc/uwsgi/apps-available/{stage}.ini /etc/uwsgi/apps-enabled/{stage}.ini".format(**config))
-        # run("/etc/init.d/uwsgi restart {stage}".format(**config))
-        cmd = "%s --http-socket :80 --pypy-home /usr/lib/pypy/bin --pypy-setup ldt/wsgi.py"
-        run()
+        fabric.contrib.files.upload_template("fab/uwsgi.ini",
+                                             "{path}/uwsgi.ini".format(**config),
+                                             context=context,
+                                             use_jinja=True)
+        run("cp -f {path}/uwsgi.ini /etc/uwsgi/apps-available/{stage}.ini".format(**config))
+        run("ln -sf /etc/uwsgi/apps-available/{stage}.ini /etc/uwsgi/apps-enabled/{stage}.ini".format(**config))
+        run("/etc/init.d/uwsgi restart {stage}".format(**config))
 
         print(green("Uploading nginx config"))
+        fabric.contrib.files.upload_template("fab/nginx-site.conf",
+                                             "{path}/nginx-site.conf".format(**config),
+                                             context=context,
+                                             use_jinja=True)
         fabric.contrib.files.upload_template("fab/nginx.conf",
                                              "{path}/nginx.conf".format(**config),
                                              context=context,
                                              use_jinja=True)
-
-        run("cp -f {path}/nginx.conf /etc/nginx/sites-available/{stage}".format(**config))
+        run("cp -f {path}/nginx.conf /etc/nginx/nginx.conf".format(**config))
+        run("cp -f {path}/nginx-site.conf /etc/nginx/sites-available/{stage}".format(**config))
         run("ln -sf /etc/nginx/sites-available/{stage} /etc/nginx/sites-enabled/{stage}".format(**config))
-        run("/etc/init.d/nginx restart".format(**config))
-
-        # print(green("Syncing database."))
-        # # run("python manage.py syncdb --noinput")
+        run("rm -f /etc/nginx/sites-available/default".format(**config))
+        run("rm -f /etc/nginx/sites-enabled/default".format(**config))
+        run("sudo service nginx restart".format(**config))
 
 
 def devdeploy():
--- ldt/wsgi.py
+++ ldt/wsgi.py
@@ -2,9 +2,6 @@
 # Author: Vova Zaytsev <zaytsev@usc.edu>
 
 import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath('__file__')), ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ldt.settings")
 
 from django.core.wsgi import get_wsgi_application
