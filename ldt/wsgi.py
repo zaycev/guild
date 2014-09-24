df1a11a7248d088975412afbd8e89e4659ac2614
Zaitsev <zaytsev@usc.edu>
2014-09-23 17:24:33 -0700

    Update

--- ldt/wsgi.py
+++ ldt/wsgi.py
@@ -2,7 +2,10 @@
 # Author: Vova Zaytsev <zaytsev@usc.edu>
 
 import os
-os.path.append("/usr/local/lib/python2.7/dist-packages")
+import sys
+
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ldt.settings")
 
 from django.core.wsgi import get_wsgi_application
